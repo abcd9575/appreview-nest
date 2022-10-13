93cc0d9a90939fd911b7ca7a55221a6cbad97c83
abcd9 <abcd9@DESKTOP-SBTQMGK>
2022-10-13 18:03:42 +0900

    Review entity added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,36 +3,52 @@ import { AuthModule } from './auth/auth.module';
 import { ReviewModule } from './review/review.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
-import { User } from './users/user.entity';
-import { Comment } from './comments/Comment.entity';
-const defaultOptions = {
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { databases } from './config/databases';
 
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '1q2w3e$R',
-  database: 'appreview',
-  synchronize: true,
-  autoLoadEntities: true,
-
-};
+// const defaultOptions = {
+//   host: 'localhost',
+//   port: 3306,
+//   username: 'root',
+//   password: '1q2w3e$R',
+//   database: 'appreview',
+//   synchronize: true,
+//   autoLoadEntities: true,
+// };
 
 @Module({
   //imports: [AuthModule, UsersModule],
   //controllers: [AuthController],
   imports: [AuthModule, 
     ReviewModule, 
-    //MongooseModule.forRoot('mongodb://localhost/nest'),
-    TypeOrmModule.forRoot({
-      ...defaultOptions,
-      type: 'mysql',
-      entities: [User], //To begin using the User entity, we need to let TypeORM know about it by inserting it into the entities array in the module forRoot() method options (unless you use a static glob path):
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRootAsync({               
+      imports: [ConfigModule],                 
+      inject: [ConfigService],                 
+      useFactory: async (configService: ConfigService) =>  
+        databases.mysql(configService),
+      
+      
+        
     }),
-    TypeOrmModule.forRoot({
-      ...defaultOptions,
-      type: 'mysql',
-      entities: [Comment], //To begin using the User entity, we need to let TypeORM know about it by inserting it into the entities array in the module forRoot() method options (unless you use a static glob path):
-    })
+    //TypeOrmModule.forFeature([User]),
+    //TypeOrmModule.forFeature([Comment]),
+
+
+
+
+
+
+    // TypeOrmModule.forRoot({
+    //   ...defaultOptions,
+    //   type: 'mysql',
+    //   entities: [User], //To begin using the User entity, we need to let TypeORM know about it by inserting it into the entities array in the module forRoot() method options (unless you use a static glob path):
+    // }),
+    // TypeOrmModule.forRoot({
+    //   ...defaultOptions,
+    //   type: 'mysql',
+    //   entities: [Comment], //To begin using the User entity, we need to let TypeORM know about it by inserting it into the entities array in the module forRoot() method options (unless you use a static glob path):
+    // })
   ],
     
   //providers: [AppService],
---
+++ src/review/review.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Comment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+  
+  @Column()
+  date: string;
+
+  @Column({type : "int", width: 5})
+  star: number;
+
+  @Column()
+  like: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  review: string;
+  
+}
+// export const CommentSchema = SchemaFactory.createForClass(Comment);
